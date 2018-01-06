f06093cfd1dcec9edf3da26fb8f3c2925f0e8172
Gal Attila <radeon16@hotmail.com>
2018-01-06 19:24:23 +0200

    - added very simple wrapper for wkhtmltopdf - simple console tests

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Utilities;
+using Wrapper;
+
+namespace HtmlToPdfConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //google test
+            WkHtmlWrapper.CreatePdfFromUrl("www.google.com", "google.pdf");
+            WkHtmlWrapper.CreatePdfFromUrl("www.yahoo.com","yahoo.pdf");
+
+        }
+    }
+}
---
+++ WkHtmlWrapper/WkHtmlWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Utilities;
+
+namespace Wrapper
+{
+    public static class WkHtmlWrapper
+    {
+        public static void CreatePdfFromUrl(string url, string outputUrl = "test.pdf")
+        {
+            if (String.IsNullOrEmpty(url) || String.IsNullOrEmpty(outputUrl))
+            {
+                throw new ArgumentException("Url not supplied");
+            }
+            CommandLineProcessCreator creator = new CommandLineProcessCreator();
+            var process = creator.CreateService($"wkhtmltopdf {url} {outputUrl}");
+            process.Start();
+        }
+    }
+}
